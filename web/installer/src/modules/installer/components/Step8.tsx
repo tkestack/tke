0ed6cb8ec59f5fadf960df8265a0e4897c90afb4
simonzhzhu <simonzhzhu@tencent.com>
2019-12-10 11:43:31 +0800

    optimize installer steps

--- web/installer/src/modules/installer/components/Step8.tsx
+++ web/installer/src/modules/installer/components/Step8.tsx
@@ -1,78 +1,171 @@
 import * as React from 'react';
 import { RootProps } from './InstallerApp';
-import { Button, Input, Form, Switch, Segment } from '@tencent/tea-component';
-import { getValidateStatus } from '../../common/utils';
-import { validateActions } from '../actions/validateActions';
+import { Button, Form, Alert } from '@tencent/tea-component';
+import { OperationState, isSuccessWorkflow } from '@tencent/qcloud-redux-workflow';
+import { getWorkflowError } from '../../common/utils';
 
 export class Step8 extends React.Component<RootProps> {
   render() {
-    const { actions, editState, step } = this.props;
+    const { actions, editState, step, createCluster } = this.props;
+    let failed = createCluster.operationState === OperationState.Done && !isSuccessWorkflow(createCluster);
+
     return step === 'step8' ? (
       <section>
+        <Form.Title>基本配置</Form.Title>
         <Form>
-          <Form.Item label="是否开启" message="建议默认开启，否则平台将不会安装控制台，仅可使用命令行工具或API管理集群">
-            <Switch
-              value={editState.openConsole}
-              onChange={value => actions.installer.updateEdit({ openConsole: value })}
-            />
+          <Form.Item label="用户名">
+            <Form.Text>{editState.username}</Form.Text>
           </Form.Item>
-          {editState.openConsole ? (
+          <Form.Item label="密码">
+            <Form.Text>{editState.password}</Form.Text>
+          </Form.Item>
+        </Form>
+        <hr />
+        <Form.Title>高可用设置</Form.Title>
+        <Form>
+          <Form.Item label="高可用类型">
+            <Form.Text>
+              {editState.haType === 'tke' ? 'TKE提供' : editState.haType === 'thirdParty' ? '使用已有' : '不设置'}
+            </Form.Text>
+          </Form.Item>
+          {editState.haType === 'tke' ? (
+            <Form.Item label="VIP地址">
+              <Form.Text>{editState.haTkeVip}</Form.Text>
+            </Form.Item>
+          ) : editState.haType === 'thirdParty' ? (
+            <Form.Item label="VIP地址">
+              <Form.Text>{editState.haThirdVip}</Form.Text>
+            </Form.Item>
+          ) : (
+            ''
+          )}
+        </Form>
+        <hr />
+        <Form.Title>集群设置</Form.Title>
+        <Form>
+          <Form.Item label="网卡名称">
+            <Form.Text>{editState.networkDevice}</Form.Text>
+          </Form.Item>
+          <Form.Item label="GPU类型">
+            <Form.Text>{editState.gpuType === 'none' ? '不使用' : editState.gpuType}</Form.Text>
+          </Form.Item>
+          <Form.Item label="容器网络">
+            <Form.Text>{editState.cidr}</Form.Text>
+          </Form.Item>
+          <Form.Item label="master节点">
+            {editState.machines.map(m => (
+              <Form.Text>{m.host}</Form.Text>
+            ))}
+          </Form.Item>
+        </Form>
+        <hr />
+        <Form.Title>认证模块设置</Form.Title>
+        <Form>
+          <Form.Item label="认证方式">
+            <Form.Text>{editState.authType}</Form.Text>
+          </Form.Item>
+          {editState.authType === 'oidc' ? (
             [
-              <Form.Item label="控制台域名">
-                <Input
-                  value={editState.consoleDomain}
-                  onChange={value => actions.installer.updateEdit({ consoleDomain: value })}
-                />
+              <Form.Item label="IssueUrl">
+                <Form.Text>{editState.issueURL}</Form.Text>
               </Form.Item>,
-              <Form.Item label="证书类型">
-                <Segment
-                  value={editState.certType}
-                  options={[
-                    { text: '自签名证书', value: 'selfSigned' },
-                    { text: '指定服务端证书', value: 'thirdParty' }
-                  ]}
-                  onChange={value => actions.installer.updateEdit({ certType: value })}
-                />
-                {editState.certType === 'thirdParty' ? (
-                  <div className="run-docker-box" style={{ marginTop: '10px', width: '100%' }}>
-                    <Form>
-                      <Form.Item
-                        label="证书"
-                        required
-                        status={getValidateStatus(editState.v_certificate)}
-                        message={editState.v_certificate.message}
-                      >
-                        <Input
-                          style={{ width: '400px' }}
-                          multiline
-                          value={editState.certificate}
-                          onChange={value => actions.installer.updateEdit({ certificate: value })}
-                        />
-                      </Form.Item>
-                      <Form.Item
-                        label="私钥"
-                        required
-                        status={getValidateStatus(editState.v_privateKey)}
-                        message={editState.v_privateKey.message}
-                      >
-                        <Input
-                          style={{ width: '400px' }}
-                          multiline
-                          value={editState.privateKey}
-                          onChange={value => actions.installer.updateEdit({ privateKey: value })}
-                        />
-                      </Form.Item>
-                    </Form>
-                  </div>
-                ) : (
-                  <noscript />
-                )}
+              <Form.Item label="ClientID">
+                <Form.Text>{editState.clientID}</Form.Text>
+              </Form.Item>,
+              <Form.Item label="CA证书">
+                <Form.Text>{editState.caCert}</Form.Text>
               </Form.Item>
             ]
           ) : (
             <noscript />
           )}
         </Form>
+        <hr />
+        <Form.Title>镜像仓库设置</Form.Title>
+        <Form>
+          <Form.Item label="镜像仓库类型">
+            <Form.Text>{editState.repoType}</Form.Text>
+          </Form.Item>
+          {editState.repoType === 'tke' ? (
+            [
+              <Form.Item label="域名后缀">
+                <Form.Text>{editState.repoSuffix}</Form.Text>
+              </Form.Item>
+            ]
+          ) : editState.repoType === 'thirdParty' ? (
+            [
+              <Form.Item label="仓库地址">
+                <Form.Text>{editState.repoAddress}</Form.Text>
+              </Form.Item>,
+              <Form.Item label="命名空间">
+                <Form.Text>{editState.repoNamespace}</Form.Text>
+              </Form.Item>,
+              <Form.Item label="用户名">
+                <Form.Text>{editState.repoUser}</Form.Text>
+              </Form.Item>,
+              <Form.Item label="密码">
+                <Form.Text>{editState.repoPassword}</Form.Text>
+              </Form.Item>
+            ]
+          ) : (
+            <noscript />
+          )}
+        </Form>
+        <hr />
+        <Form.Title>业务模块设置</Form.Title>
+        <Form>
+          <Form.Item label="是否开启">
+            <Form.Text>{editState.openBusiness ? '是' : '否'}</Form.Text>
+          </Form.Item>
+        </Form>
+        <hr />
+        <Form.Title>监控模块设置</Form.Title>
+        <Form>
+          <Form.Item label="监控存储类型">
+            <Form.Text>{editState.monitorType}</Form.Text>
+          </Form.Item>
+          {editState.monitorType === 'es' ? (
+            [
+              <Form.Item label="ES地址">
+                <Form.Text>{editState.esUrl}</Form.Text>
+              </Form.Item>,
+              <Form.Item label="用户名">
+                <Form.Text>{editState.esUsername}</Form.Text>
+              </Form.Item>,
+              <Form.Item label="密码">
+                <Form.Text>{editState.esPassword}</Form.Text>
+              </Form.Item>
+            ]
+          ) : editState.monitorType === 'external-inflexdb' ? (
+            [
+              <Form.Item label="InfluxDB地址">
+                <Form.Text>{editState.influxDBUrl}</Form.Text>
+              </Form.Item>,
+              <Form.Item label="用户名">
+                <Form.Text>{editState.influxDBUsername}</Form.Text>
+              </Form.Item>,
+              <Form.Item label="密码">
+                <Form.Text>{editState.influxDBPassword}</Form.Text>
+              </Form.Item>
+            ]
+          ) : (
+            <noscript />
+          )}
+        </Form>
+        <hr />
+        <Form.Title>控制台设置</Form.Title>
+        <Form>
+          <Form.Item label="是否开启">
+            <Form.Text>{editState.openConsole ? '是' : '否'}</Form.Text>
+          </Form.Item>
+          <Form.Item label="控制台域名">
+            <Form.Text>{editState.consoleDomain || '无'}</Form.Text>
+          </Form.Item>
+          <Form.Item label="证书类型">
+            <Form.Text>{editState.certType}</Form.Text>
+          </Form.Item>
+        </Form>
+
         <Form.Action style={{ position: 'absolute', bottom: '20px', left: '20px', width: '960px' }}>
           <Button style={{ marginRight: '10px' }} type="weak" onClick={() => actions.installer.stepNext('step7')}>
             上一步
@@ -80,14 +173,24 @@ export class Step8 extends React.Component<RootProps> {
           <Button
             type="primary"
             onClick={() => {
-              actions.validate.validateStep8(editState);
-              if (validateActions._validateStep8(editState)) {
-                actions.installer.stepNext('step9');
-              }
+              actions.installer.createCluster.start([editState]);
+              actions.installer.createCluster.perform();
             }}
           >
-            下一步
+            安装
           </Button>
+          {failed && (
+            <Alert
+              type="error"
+              style={{
+                display: 'inline-block',
+                marginTop: '10px',
+                marginBottom: '0px'
+              }}
+            >
+              {getWorkflowError(createCluster)}
+            </Alert>
+          )}
         </Form.Action>
       </section>
     ) : (
