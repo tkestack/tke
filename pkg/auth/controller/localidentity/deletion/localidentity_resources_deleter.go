a73631124cbdd3e708fa00b59aa6d42367c9fd5c
liangyuzhou <liangyuzhou@tencent.com>
2019-12-25 16:52:45 +0800

    fix: change imported package name

--- pkg/auth/controller/localidentity/deletion/localidentity_resources_deleter.go
+++ pkg/auth/controller/localidentity/deletion/localidentity_resources_deleter.go
@@ -20,17 +20,16 @@ package deletion
 
 import (
 	"fmt"
-	"k8s.io/apimachinery/pkg/fields"
-	"strings"
-	"tkestack.io/tke/pkg/auth/util"
-
 	"github.com/casbin/casbin/v2"
 	"k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/fields"
 	utilerrors "k8s.io/apimachinery/pkg/util/errors"
 	"k8s.io/apimachinery/pkg/util/sets"
-	v1 "tkestack.io/tke/api/auth/v1"
+	"strings"
+	"tkestack.io/tke/api/auth/v1"
 	v1clientset "tkestack.io/tke/api/client/clientset/versioned/typed/auth/v1"
+	"tkestack.io/tke/pkg/auth/util"
 	"tkestack.io/tke/pkg/util/log"
 )
 
