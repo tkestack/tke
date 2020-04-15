736779a9ad103cf544f434b0f2ca96e65701b221
tfan <tfan@tencent.com>
2020-04-15 22:25:08 +0800

    feat: Add support of capabilities add/drop for container

--- web/console/src/modules/cluster/constants/Config.ts
+++ web/console/src/modules/cluster/constants/Config.ts
@@ -723,3 +723,50 @@ export enum MachineStatus {
   Failed = 'Failed',
   Terminating = 'Terminating',
 }
+
+/** 增加 Capabilities 选项 */
+export const AddCapabilitiesList = [
+  'SYS_MODULE',
+  'SYS_RAWIO',
+  'SYS_PACCT',
+  'SYS_ADMIN',
+  'SYS_NICE',
+  'SYS_RESOURCE',
+  'SYS_TIME',
+  'SYS_TTY_CONFIG',
+  'AUDIT_CONTROL',
+  'MAC_OVERRIDE',
+  'MAC_ADMIN',
+  'NET_ADMIN',
+  'SYSLOG',
+  'DAC_READ_SEARCH',
+  'LINUX_IMMUTABLE',
+  'NET_BROADCAST',
+  'IPC_LOCK',
+  'IPC_OWNER',
+  'SYS_PTRACE',
+  'SYS_BOOT',
+  'LEASE',
+  'WAKE_ALARM',
+  'BLOCK_SUSPEND',
+  'all',
+];
+
+/** 删除 Capabilities 选项 */
+export const DropCapabilitiesList = [
+  'SETPCAP',
+  'MKNOD',
+  'AUDIT_WRITE',
+  'CHOWN',
+  'NET_RAW',
+  'DAC_OVERRIDE',
+  'FOWNER',
+  'FSETID',
+  'KILL',
+  'SETGID',
+  'SETUID',
+  'NET_BIND_SERVICE',
+  'SYS_CHROOT',
+  'SETFCAP',
+  'all',
+];
