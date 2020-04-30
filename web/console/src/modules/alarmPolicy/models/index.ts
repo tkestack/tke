acb80d0b585329d305332fbccf8441af90ddf715
kaychen <kaychen@tencent.com>
2020-04-30 11:58:53 +0800

    feat: alarmplicy support tapp

--- web/console/src/modules/alarmPolicy/models/index.ts
+++ web/console/src/modules/alarmPolicy/models/index.ts
@@ -6,3 +6,4 @@ export { ClusterFilter } from './ClusterFilter';
 export { AlarmPolicy, AlarmPolicyEdition, AlarmPolicyFilter, AlarmPolicyOperator } from './AlarmPolicy';
 export { Group, GroupUser } from './Group';
 export { Namespace, NamespaceFilter } from './Namespace';
+export { AddonStatus } from './Addon';
