d4c0c9d2096359b9bb165e74eca6188c08305ab5
jianzhuang <jianzhuang@tencent.com>
2020-08-03 10:37:19 +0800

    fix: support default chartgroup

--- cmd/tke-registry-controller/app/options/registry.go
+++ cmd/tke-registry-controller/app/options/registry.go
@@ -19,9 +19,12 @@
 package options
 
 import (
+	"github.com/spf13/pflag"
+	"github.com/spf13/viper"
 	registryconfig "tkestack.io/tke/pkg/registry/apis/config"
 	registryscheme "tkestack.io/tke/pkg/registry/apis/config/scheme"
 	registryconfigv1 "tkestack.io/tke/pkg/registry/apis/config/v1"
+	registrycontrollerconfig "tkestack.io/tke/pkg/registry/controller/config"
 )
 
 // NewRegistryConfiguration will create a new RegistryConfiguration with default values
@@ -37,4 +40,50 @@ func NewRegistryConfiguration() (*registryconfig.RegistryConfiguration, error) {
 		return nil, err
 	}
 	return config, nil
+}
+
+const (
+	flagDefaultSystemChartGroups = "registry-setting-default-system-chartgroups"
+)
+
+const (
+	configDefaultSystemChartGroups = "registry_setting.default_system_chartgroups"
+)
+
+// RegistryOptions contains configuration items related to registry attributes.
+type RegistryOptions struct {
+	DefaultSystemChartGroups []string
+}
+
+// NewRegistryOptions creates a RegistryOptions object with default parameters.
+func NewRegistryOptions() *RegistryOptions {
+	return &RegistryOptions{}
+}
+
+// AddFlags adds flags for console to the specified FlagSet object.
+func (o *RegistryOptions) AddFlags(fs *pflag.FlagSet) {
+	fs.StringSlice(flagDefaultSystemChartGroups, o.DefaultSystemChartGroups,
+		"Default chartgroups with system type and public visibility.")
+	_ = viper.BindPFlag(configDefaultSystemChartGroups, fs.Lookup(flagDefaultSystemChartGroups))
+}
+
+// ApplyFlags parsing parameters from the command line or configuration file
+// to the options instance.
+func (o *RegistryOptions) ApplyFlags() []error {
+	var errs []error
+
+	o.DefaultSystemChartGroups = viper.GetStringSlice(configDefaultSystemChartGroups)
+
+	return errs
+}
+
+// ApplyTo fills up Debugging config with options.
+func (o *RegistryOptions) ApplyTo(cfg *registrycontrollerconfig.RegistryDefaultConfiguration) error {
+	if o == nil {
+		return nil
+	}
+
+	cfg.DefaultSystemChartGroups = o.DefaultSystemChartGroups[:]
+
+	return nil
 }
