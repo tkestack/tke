380e87371c0a2496398345892bebce447d757a22
jianzhuang <jianzhuang@tencent.com>
2020-08-04 14:34:22 +0800

    fix(registry): registry url sub

--- web/console/src/modules/registry/router.ts
+++ web/console/src/modules/registry/router.ts
@@ -4,9 +4,8 @@ import { Router } from '../../../helpers/Router';
  * @param sub   二级菜单栏的一级导航
  * @param mode  当前的展示内容类型 list | create | update | detail
  * @param tab   详情页 tab
- * @param name  镜像仓库键值
  */
-export const router = new Router('/tkestack/registry/appcenter(/:sub)(/:mode)(/:tab)', {
+export const router = new Router('/tkestack/registry(/:sub)(/:mode)(/:tab)', {
   sub: '',
   mode: '',
   tab: '',
