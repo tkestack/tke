1276cce649fbf21a384c840ac4e5374f35c132f5
leonarliu <leonarliu@tencent.com>
2020-08-27 10:56:36 +0800

    feat(gateway): support component openapi overview

---
+++ pkg/gateway/proxy/handler/rewriteproxy/handler.go
@@ -0,0 +1,97 @@
+/*
+ * Tencent is pleased to support the open source community by making TKEStack
+ * available.
+ *
+ * Copyright (C) 2012-2019 Tencent. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use
+ * this file except in compliance with the License. You may obtain a copy of the
+ * License at
+ *
+ * https://opensource.org/licenses/Apache-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OF ANY KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
+
+package rewriteproxy
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+	"net/http/httputil"
+	"net/url"
+	"strings"
+
+	"k8s.io/apimachinery/pkg/api/errors"
+	netutil "k8s.io/apimachinery/pkg/util/net"
+	"k8s.io/apiserver/pkg/endpoints/handlers/responsewriters"
+	gatewayconfig "tkestack.io/tke/pkg/gateway/apis/config"
+	"tkestack.io/tke/pkg/gateway/token"
+	"tkestack.io/tke/pkg/util/log"
+	"tkestack.io/tke/pkg/util/transport"
+)
+
+type handler struct {
+	reverseProxy    *httputil.ReverseProxy
+	protected       bool
+	rewritePathFunc RewritePathFunc
+}
+
+// RewritePathFunc rewrites url path.
+type RewritePathFunc func(path string) (newPath string)
+
+// NewHandler to create a reverse proxy handler and returns it.
+// The reverse proxy will rewrite url path .
+func NewHandler(address string, cfg *gatewayconfig.PassthroughComponent, protected bool, rewritePathFunc RewritePathFunc) (http.Handler, error) {
+	u, err := url.Parse(address)
+	if err != nil {
+		log.Error("Failed to parse backend service address", log.String("address", address), log.Err(err))
+		return nil, err
+	}
+
+	tr, err := transport.NewOneWayTLSTransport(cfg.CAFile, true)
+	if err != nil {
+		log.Error("Failed to create one-way HTTPS transport", log.String("caFile", cfg.CAFile), log.Err(err))
+		return nil, err
+	}
+
+	reverseProxy := httputil.NewSingleHostReverseProxy(&url.URL{Scheme: u.Scheme, Host: u.Host})
+	reverseProxy.Transport = tr
+	reverseProxy.ErrorLog = log.StdErrLogger()
+	return &handler{reverseProxy, protected, rewritePathFunc}, nil
+}
+
+func (h *handler) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+	newPath := h.rewritePathFunc(req.URL.Path)
+	u, err := url.Parse(newPath)
+	if err != nil {
+		log.Error("Failed to parse new path", log.String("newPath", newPath), log.Err(err))
+		responsewriters.InternalError(w, req, err)
+	}
+	req.URL.Path = u.Path
+	if u.RawQuery != "" {
+		req.URL.RawQuery = u.RawQuery
+	}
+
+	if h.protected {
+		// read cookie
+		t, err := token.RetrieveToken(req)
+		if err != nil {
+			log.Error("Failed to retrieve token from client", log.Err(err))
+			responsewriters.WriteRawJSON(http.StatusUnauthorized, errors.NewUnauthorized(err.Error()), w)
+			return
+		}
+		log.Debug("Reverse proxy to protected backend component", log.String("url", req.URL.Path))
+		newReq := req.WithContext(context.Background())
+		newReq.Header = netutil.CloneHeader(req.Header)
+		newReq.Header.Set("Authorization", fmt.Sprintf("Bearer %s", strings.TrimSpace(t.ID)))
+		h.reverseProxy.ServeHTTP(w, newReq)
+		return
+	}
+	log.Debug("Reverse proxy to unprotected backend component", log.String("url", req.URL.Path))
+	h.reverseProxy.ServeHTTP(w, req)
+}
