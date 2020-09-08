daab1317620a18889c5857fa73fc49a6625929fe
kaychen <kaychen@tencent.com>
2020-09-08 19:43:45 +0800

    fix: fix ns selection change after refresh

--- web/console/src/modules/cluster/actions/namespaceActions.project.ts
+++ web/console/src/modules/cluster/actions/namespaceActions.project.ts
@@ -74,7 +74,7 @@ const restActions = {
 
       dispatch({
         type: ActionType.SelectNamespace,
-        payload: namespace
+        payload: finder.metadata.name
       });
 
       // 这里进行路由的更新，如果不需要命名空间的话，路由就不需要有np的信息
