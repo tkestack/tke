2a205c983227db09f31c1a1f86ffd06173f0672e
huxiaoliang <vvictorhu@tencent.com>
2020-09-14 17:09:34 +0800

    fix(console): fix CI can't pick up installer web commit

--- web/installer/webpack/webpack.prod.js
+++ web/installer/webpack/webpack.prod.js
@@ -13,7 +13,7 @@ module.exports = {
   },
 
   output: {
-    path: path.resolve(__dirname, '../public'),
+    path: path.resolve(__dirname, '../build'),
     filename: 'static/js/installer.js'
   },
 
