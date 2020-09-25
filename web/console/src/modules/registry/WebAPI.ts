5839bdc7943a7f1406151fda9dcb39f493e75beb
jianzhuang <jianzhuang@tencent.com>
2020-09-25 10:10:52 +0800

    fix(console): update registry webapi projectid

--- web/console/src/modules/registry/WebAPI.ts
+++ web/console/src/modules/registry/WebAPI.ts
@@ -12,8 +12,7 @@ import {
   POST,
   PUT,
   PATCH,
-  DELETE,
-  getProjectName
+  DELETE
 } from '../../../helpers';
 import { resourceConfig } from '../../../config/resourceConfig';
 import { RequestParams, ResourceInfo } from '../common/models';
@@ -511,7 +510,7 @@ export async function fetchChartGroupList(query: QueryState<ChartGroupFilter>) {
   const queryString = reduceK8sQueryString({ k8sQueryObj: queryObj });
   // TODO FIX
   // reduceNetworkRequest设置了未传业务id则从cookie中读取的逻辑，传空的业务id是为了适配这段逻辑
-  let rr: RequestResult = await GET({ url: url + queryString, keyword: keyword, projectId: getProjectName() });
+  let rr: RequestResult = await GET({ url: url + queryString, keyword: keyword, projectId: '' });
   let objs: ChartGroup[] = !rr.error && rr.data.items ? rr.data.items : [];
   const result: RecordSet<ChartGroup> = {
     recordCount: objs.length,
@@ -697,7 +696,7 @@ export async function fetchChartList(query: QueryState<ChartFilter>) {
   // reduceNetworkRequest设置了未传业务id则从cookie中读取的逻辑，传空的业务id是为了适配这段逻辑
   let rr: RequestResult = await GET({
     url: url + queryString,
-    projectId: filter.projectID ? filter.projectID : getProjectName(),
+    projectId: filter.projectID ? filter.projectID : '',
     keyword
   });
   let objs: Chart[] = !rr.error && rr.data.items ? rr.data.items : [];
