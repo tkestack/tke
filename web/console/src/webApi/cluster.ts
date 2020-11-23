bef04e6ba70d0eb7be184dce8301307a46f44f4d
johnny19941216 <wuzhiqiang94@hotmail.com>
2020-11-23 16:09:14 +0800

    feat(console): add update api

--- web/console/src/webApi/cluster.ts
+++ web/console/src/webApi/cluster.ts
@@ -1,5 +1,5 @@
 import Request from './request';
 
 export const getK8sValidVersions = () => {
-  return Request.get('/v1/namespaces/kube-public/configmaps/cluster-info');
+  return Request.get<any, { k8sValidVersions: Array<string> }>('/v1/namespaces/kube-public/configmaps/cluster-info');
 };
