b2ce175d9b6438ea131180e0ed691875172b24e3
johnny19941216 <wuzhiqiang94@hotmail.com>
2020-11-23 11:58:31 +0800

    feat(console): add import cluster from kubeconfig

---
+++ web/console/src/modules/cluster/components/clusterManage/KubeconfigFileParse.tsx
@@ -0,0 +1,117 @@
+import React, { useState } from 'react';
+import { Upload, Text } from '@tea/component';
+import * as yaml from 'js-yaml';
+
+export interface KubeConfig {
+  apiVersion: string;
+  clusters: Array<{
+    cluster: {
+      'certificate-authority-data': string;
+      server: string;
+    };
+    name: string;
+  }>;
+  'current-context': string;
+  kind: string;
+  preferences: {};
+  users: Array<{
+    name: string;
+    user: {
+      token?: string;
+      'client-certificate-data'?: string;
+      'client-key-data'?: string;
+    };
+  }>;
+}
+
+export interface KubeconfigFileParseProps {
+  onSuccess: (targetConfig: {
+    apiServer: string;
+    certFile: string;
+    token: string;
+    clientCert: string;
+    clientKey: string;
+  }) => any;
+}
+
+export function KubeconfigFileParse({ onSuccess }: KubeconfigFileParseProps) {
+  const [errorMessage, setErrorMessage] = useState('');
+
+  function beforeUpload(file: File) {
+    setErrorMessage('');
+    fileParse(file)
+      .then((rsp: KubeConfig) => {
+        console.log(rsp);
+        onSuccess(yamlToConfig(rsp));
+      })
+      .catch(error => {
+        onFiled(error);
+      });
+
+    return false;
+  }
+
+  function onFiled(error: Error) {
+    console.log(error);
+    setErrorMessage(error.message);
+  }
+
+  function fileParse(file: File) {
+    return new Promise((resolve, reject) => {
+      const reader = new FileReader();
+      reader.onload = ({ target }: any) => {
+        if (target.readyState === 2) {
+          let rsp;
+          try {
+            rsp = yaml.load(target.result as string);
+          } catch (error) {
+            return reject(new Error('文件解析失败'));
+          }
+
+          if (rsp.kind !== 'Config') {
+            return reject(new Error('这不是一个kubeconfig文件'));
+          }
+
+          return resolve(rsp);
+        } else {
+          return reject(new Error('文件解析失败'));
+        }
+      };
+
+      reader.readAsText(file);
+    });
+  }
+
+  function yamlToConfig({
+    clusters: [
+      {
+        cluster: { server, 'certificate-authority-data': certFile }
+      }
+    ],
+    users: [
+      {
+        user: { token, 'client-certificate-data': clientCert, 'client-key-data': clientKey }
+      }
+    ]
+  }: KubeConfig) {
+    return {
+      apiServer: server,
+      certFile,
+      token,
+      clientCert,
+      clientKey
+    };
+  }
+
+  return (
+    <Upload beforeUpload={beforeUpload}>
+      <a style={{ fontSize: '12px' }}>点击上传kubeconfig文件</a>
+      <br />
+      {errorMessage && (
+        <Text style={{ fontSize: '12px' }} theme="danger">
+          {errorMessage}
+        </Text>
+      )}
+    </Upload>
+  );
+}
