9d9cd058aa101e973a2dbbdb7c94345426a41114
johnny19941216 <wuzhiqiang94@hotmail.com>
2020-11-24 21:27:36 +0800

    feat(console): add cluster update

--- web/console/src/modules/cluster/components/clusterManage/ClusterUpdate.tsx
+++ web/console/src/modules/cluster/components/clusterManage/ClusterUpdate.tsx
@@ -2,12 +2,32 @@ import React, { useEffect, useState } from 'react';
 import { Space, Button, Form, Select, Checkbox, InputNumber, Typography } from 'antd';
 import { AntdLayout } from '@src/modules/common/layouts';
 import { getK8sValidVersions } from '@src/webApi/cluster';
-import compareVersion from 'compare-versions';
+import { compare } from 'compare-versions';
+import { RootProps } from '../ClusterApp';
+import { updateCluster } from '@src/webApi/cluster';
 
-export function ClusterUpdate() {
+export function ClusterUpdate({ route }: RootProps) {
   const ItemStyle = () => ({
     width: 120
   });
+
+  const defaultUpgradeConfig = {
+    version: null,
+    drainNodeBeforeUpgrade: false,
+    maxUnready: 0,
+    autoMode: false
+  };
+
+  const { clusterId, clusterVersion } = route.queries;
+
+  function goBack() {
+    history.back();
+  }
+
+  async function perform(values) {
+    await updateCluster({ ...values, clusterName: clusterId });
+    goBack();
+  }
 
   console.log(React.version);
 
@@ -15,8 +35,8 @@ export function ClusterUpdate() {
 
   useEffect(() => {
     async function fetchK8sValidVersions() {
-      const { k8sValidVersions } = await getK8sValidVersions();
-      setK8sValidVersions(k8sValidVersions);
+      const versions = await getK8sValidVersions();
+      setK8sValidVersions(versions);
     }
 
     fetchK8sValidVersions();
@@ -27,38 +47,68 @@ export function ClusterUpdate() {
       title="升级Master"
       footer={
         <Space>
-          <Button type="primary" htmlType="submit" form="promethusConfigForm">
+          <Button type="primary" htmlType="submit" form="clusterUpdateConfigForm">
             提交
           </Button>
-          <Button>取消</Button>
+          <Button onClick={goBack}>取消</Button>
         </Space>
       }
     >
       <Typography.Title level={5} style={{ marginBottom: 35 }}>
-        更新集群的K8S版本1.14.3至：
+        更新集群的K8S版本{clusterVersion}至：
       </Typography.Title>
 
-      <Form labelAlign="left" labelCol={{ span: 3 }} size="middle">
+      <Form
+        id="clusterUpdateConfigForm"
+        labelAlign="left"
+        labelCol={{ span: 3 }}
+        size="middle"
+        validateTrigger="onBlur"
+        initialValues={defaultUpgradeConfig}
+        onFinish={perform}
+      >
         <Form.Item
           label="升级目标版本"
-          extra="注意：master升级支持一个此版本升级到下一个次版本，或者同样次版本的补丁版。"
+          extra="注意：master升级支持一个次版本升级到下一个次版本，或者同样次版本的补丁版。"
+          name={['version']}
+          rules={[{ required: true }]}
         >
           <Select style={ItemStyle()}>
             {k8sValidVersions.map(v => (
-              <Select.Option value={v}>{v}</Select.Option>
+              <Select.Option key={v} disabled={compare(clusterVersion, v, '>=')} value={v}>
+                {v}
+              </Select.Option>
             ))}
           </Select>
         </Form.Item>
 
         <Form.Item
+          valuePropName="checked"
+          name={['drainNodeBeforeUpgrade']}
+          label="驱逐节点"
+          extra="若选择升级前驱逐节点，该节点所有pod将在升级前被驱逐，此时节点如有pod使用emptyDir类卷会导致驱逐失败而影响升级流程"
+        >
+          <Checkbox>驱逐节点</Checkbox>
+        </Form.Item>
+
+        <Form.Item
+          valuePropName="checked"
+          name={['autoMode']}
           label="自动升级Worker"
           extra="注意：启用自定升级Worker，在升级完Master后，将自动升级集群下所有Worker节点。"
         >
           <Checkbox>启用自动升级</Checkbox>
         </Form.Item>
-        <Form.Item label="最大不可用Pod占比" extra="升级过程中不可以Pod数超过该占比将暂停升级">
+
+        <Form.Item
+          label="最大不可用Pod占比"
+          extra="注意如果节点过少，而设置比例过低，没有足够多的节点承载pod的迁移会导致升级卡死。如果业务对pod可用比例较高，请考虑选择升级前不驱逐节点。"
+        >
           <Space>
-            <InputNumber style={ItemStyle()} min={0} max={100} /> %
+            <Form.Item name={['maxUnready']} noStyle rules={[{ type: 'number', required: true, min: 0, max: 100 }]}>
+              <InputNumber style={ItemStyle()} min={0} max={100} />
+            </Form.Item>
+            %
           </Space>
         </Form.Item>
       </Form>
---
+++ web/console/src/modules/cluster/components/clusterManage/WorkerUpdate.tsx
@@ -0,0 +1,153 @@
+import React, { useEffect, useState } from 'react';
+import { Space, Button, Form, Select, Checkbox, InputNumber, Typography, Transfer, Table } from 'antd';
+import { AntdLayout } from '@src/modules/common/layouts';
+import { TransferProps } from 'antd/lib/transfer';
+import { difference } from 'lodash';
+import { RootProps } from '../ClusterApp';
+import { getNodes, updateWorkers } from '@src/webApi/cluster';
+
+export function WorkerUpdate({ route }: RootProps) {
+  const ItemStyle = () => ({
+    width: 120
+  });
+
+  const [nodes, setNodes] = useState([]);
+  const [targetKeys, setTargatKeys] = useState([]);
+  const [maxUnready, setMaxUnready] = useState(0);
+
+  const { clusterId, clusterVersion } = route.queries;
+
+  useEffect(() => {
+    (async function () {
+      console.log('start:---');
+      const nodes = await getNodes({ clusterName: clusterId, clusterVersion });
+      console.log('getMachines:', nodes);
+      setNodes(nodes);
+    })();
+  }, [clusterId, clusterVersion]);
+
+  const transferColumns = [
+    {
+      dataIndex: 'name',
+      title: 'ID/名称'
+    },
+
+    {
+      dataIndex: 'labels',
+      title: 'label'
+    },
+
+    {
+      dataIndex: 'kubeletVersion',
+      title: 'Kubernetes版本'
+    },
+
+    {
+      dataIndex: 'clusterVersion',
+      title: '目标Kubernetes版本'
+    }
+  ];
+
+  async function submit() {
+    const mchineNames = [
+      ...new Set(
+        nodes
+          .filter(key => targetKeys.includes(key))
+          .map(({ machines }) => machines as string[])
+          .reduce((all, current) => [...all, ...current], [])
+      )
+    ];
+
+    await updateWorkers({
+      mchineNames,
+      maxUnready,
+      clusterName: clusterId
+    });
+  }
+
+  return (
+    <AntdLayout
+      title="升级Worker"
+      footer={
+        <Space>
+          <Button type="primary" onClick={submit}>
+            提交
+          </Button>
+          <Button>取消</Button>
+        </Space>
+      }
+    >
+      <Form labelAlign="left" labelCol={{ span: 3 }} size="middle">
+        <Form.Item label=" 升级说明">
+          当前所选集群Master版本为1.16.3，您可为您的节点Kubernetes版本升级到当前的最新版本。
+        </Form.Item>
+
+        <Form.Item label="选择节点">
+          <TableTransfer
+            columns={transferColumns}
+            dataSource={nodes}
+            targetKeys={targetKeys}
+            listStyle={{}}
+            onChange={targetKeys => setTargatKeys(targetKeys)}
+          />
+        </Form.Item>
+
+        <Form.Item label="最大不可用Pod占比" extra="升级过程中不可以Pod数超过该占比将暂停升级">
+          <Space>
+            <InputNumber
+              style={ItemStyle()}
+              min={0}
+              max={100}
+              defaultValue={maxUnready}
+              onChange={value => setMaxUnready(+value)}
+            />
+            %
+          </Space>
+        </Form.Item>
+      </Form>
+    </AntdLayout>
+  );
+}
+
+interface TableTransferProps extends TransferProps<any> {
+  columns: Array<any>;
+}
+
+function TableTransfer({ columns, ...restProps }: TableTransferProps) {
+  return (
+    <Transfer {...restProps} showSelectAll={false}>
+      {({ filteredItems, onItemSelectAll, onItemSelect, selectedKeys: listSelectedKeys, disabled: listDisabled }) => {
+        const rowSelection = {
+          getCheckboxProps: item => ({ disabled: listDisabled || item.disabled }),
+          onSelectAll(selected, selectedRows) {
+            const treeSelectedKeys = selectedRows.filter(item => !item.disabled).map(({ key }) => key);
+            const diffKeys = selected
+              ? difference(treeSelectedKeys, listSelectedKeys)
+              : difference(listSelectedKeys, treeSelectedKeys);
+            onItemSelectAll(diffKeys, selected);
+          },
+          onSelect({ key }, selected) {
+            onItemSelect(key, selected);
+          },
+          selectedRowKeys: listSelectedKeys
+        };
+
+        return (
+          <Table
+            rowSelection={rowSelection}
+            columns={columns}
+            dataSource={filteredItems}
+            size="small"
+            style={{ pointerEvents: listDisabled ? 'none' : null }}
+            onRow={({ key, disabled: itemDisabled }) => ({
+              onClick: () => {
+                if (itemDisabled || listDisabled) return;
+                onItemSelect(key, !listSelectedKeys.includes(key));
+              }
+            })}
+          />
+        );
+      }}
+    </Transfer>
+  );
+}
