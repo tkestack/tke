5960c71b0c96555e9bbdd98a79f4aa61761a1743
leonarliu <leonarliu@tencent.com>
2020-12-04 14:19:35 +0800

    feat(installer): support certificate chain

--- cmd/tke-installer/app/installer/installer_test.go
+++ cmd/tke-installer/app/installer/installer_test.go
@@ -56,7 +56,7 @@ func TestTKE_validateCertAndKey(t *testing.T) {
 	err := tke.validateCertAndKey(
 		pem.EncodeToMemory(&pem.Block{Type: "CERTIFICATE", Bytes: cert.Raw}),
 		pem.EncodeToMemory(&pem.Block{Type: "RSA PRIVATE KEY", Bytes: x509.MarshalPKCS1PrivateKey(key)}),
-		[]string{"console.tke.com", "registry.tke.com", "*.registry.tke.com"},
+		"console.tke.com",
 	)
 	assert.True(t, err == nil)
 }
