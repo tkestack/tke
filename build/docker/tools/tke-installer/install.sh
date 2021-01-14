6997ea8e303704439956e456da0c80937ebb8687
Leo Ryu <leonarliu@tencent.com>
2021-01-14 17:25:33 +0800

    fix(installer): delet custom resource first time (#1056)

--- build/docker/tools/tke-installer/install.sh
+++ build/docker/tools/tke-installer/install.sh
@@ -132,7 +132,7 @@ function load_image() {
 function clean_old_data() {
   echo "Step.4 clean old data [doing]"
 
-  rm -rf $DATA_DIR >/dev/null 2>&1 || :
+  find $DATA_DIR/* |egrep -v "(custom_upgrade_resource)" | xargs rm -rf  >/dev/null 2>&1 || :
   docker rm -f tke-installer >/dev/null 2>&1 || :
   docker volume prune -f >/dev/null 2>&1 || :
 
