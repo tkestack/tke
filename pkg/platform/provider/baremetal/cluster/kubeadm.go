83d96befb095dba368242203a540fb772887502c
XiaoLiang Hu <vvictorhu@tencent.com>
2021-03-24 15:27:45 +0800

    Revert "fix(platform): fix anonymous auth (#1147)" (#1151)
    This reverts commit b85fb5cf87c6cf0f35a5792fcc18a0d910a16058.

--- pkg/platform/provider/baremetal/cluster/kubeadm.go
+++ pkg/platform/provider/baremetal/cluster/kubeadm.go
@@ -215,7 +215,6 @@ func (p *Provider) getKubeletConfiguration(c *v1.Cluster) *kubeletv1beta1.Kubele
 func (p *Provider) getAPIServerExtraArgs(c *v1.Cluster) map[string]string {
 	args := map[string]string{
 		"token-auth-file": constants.TokenFile,
-		"anonymous-auth":  "false",
 	}
 	if p.config.AuditEnabled() {
 		args["audit-policy-file"] = constants.KubernetesAuditPolicyConfigFile
