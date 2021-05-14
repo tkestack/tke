9dfbe3981d83d706bccd6525b4c7fa0a2d9e4bdd
leonarliu <leonarliu@tencent.com>
2021-05-14 11:14:49 +0800

    fix(platform): drain node err when using 1.19 api

--- api/platform/types.go
+++ api/platform/types.go
@@ -798,6 +798,7 @@ type StorageBackEndES struct {
 	ReserveDays int32
 }
 
+// +k8s:conversion-gen:explicit-from=net/url.Values
 // +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
 
 // HelmProxyOptions is the query options to a Helm-api proxy call.
