3369a9e801b79f71b74ffad96ccdcdc1c18cfec5
jo-hnny <wuzhiqiang94@hotmail.com>
2021-08-27 15:28:39 +0800

    fix(console): add license to web (#1519)
    * fix(console): add gulpfile to web
    
    * fix(console): add license to web
    
    * fix(console): rm license gulpfile

--- web/console/helpers/format.ts
+++ web/console/helpers/format.ts
@@ -1,3 +1,20 @@
+/*
+ * Tencent is pleased to support the open source community by making TKEStack
+ * available.
+ *
+ * Copyright (C) 2012-2021 Tencent. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use
+ * this file except in compliance with the License. You may obtain a copy of the
+ * License at
+ *
+ * https://opensource.org/licenses/Apache-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OF ANY KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
 // 所有单位都换算成byte
 const memoryMap1000 = ['K', 'M', 'G', 'T', 'P', 'E'].reduce(
   (all, key, index) => Object.assign(all, { [key]: Math.pow(1000, index + 1) }),
