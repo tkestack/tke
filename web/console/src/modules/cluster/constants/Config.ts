5b40abbe65e62857ff72ee68dd895ee91ddadaf9
johnny19941216 <wuzhiqiang94@hotmail.com>
2021-09-14 10:11:31 +0800

    feat(console): upgrade clear node sh

--- web/console/src/modules/cluster/constants/Config.ts
+++ web/console/src/modules/cluster/constants/Config.ts
@@ -704,29 +704,37 @@ export const LbcfArgsConfig = [
 ];
 
 export const clearNodeSH = `#!/bin/bash
-rm -rf /etc/kubernetes
 
-systemctl stop kubelet 2>/dev/null
-
-docker rm -f $(docker ps -aq) 2>/dev/null
-systemctl stop docker 2>/dev/null
-
+# common
+kubeadm reset -f
+rm -fv /root/.kube/config
+rm -rfv /etc/kubernetes
+rm -rfv /var/lib/kubelet
+rm -rfv /var/lib/etcd
+rm -rfv /var/lib/cni
+rm -rfv /etc/cni
+rm -rfv /var/lib/tke-registry-api
+rm -rfv /opt/tke-installer
+rm -rfv /var/lib/postgresql /etc/core/token /var/lib/redis /storage /chart_storage
 ip link del cni0 2>/etc/null
 
 for port in 80 2379 6443 8086 {10249..10259} ; do
     fuser -k -9 \${port}/tcp
 done
 
-rm -rfv /etc/kubernetes
+# docker
+docker rm -f $(docker ps -aq) 2>/dev/null
+systemctl disable docker 2>/dev/null
+systemctl stop docker 2>/dev/null
 rm -rfv /etc/docker
-rm -fv /root/.kube/config
-rm -rfv /var/lib/kubelet
-rm -rfv /var/lib/cni
-rm -rfv /etc/cni
-rm -rfv /var/lib/etcd
-rm -rfv /var/lib/postgresql /etc/core/token /var/lib/redis /storage /chart_storage
+ip link del docker0 2>/etc/null
 
-systemctl start docker 2>/dev/null`;
+# containerd
+nerdctl rm -f $(nerdctl ps -aq) 2>/dev/null
+ip netns list | cut -d' ' -f 1 | xargs -n1 ip netns delete 2>/dev/null
+systemctl disable containerd 2>/dev/null
+systemctl stop containerd 2>/dev/null
+rm -rfv /var/lib/nerdctl/*`;
 
 export enum GPUTYPE {
   PGPU = 'Physical',
