093dfd77fb06eec5d8219c5097f49a01102424fa
Leo Ryu <leo-ryu@outlook.com>
2021-09-21 09:59:41 +0800

    fix(installer): check installer mode from input (#1594)

--- build/docker/tools/tke-installer/install.sh
+++ build/docker/tools/tke-installer/install.sh
@@ -182,15 +182,15 @@ function start_installer() {
 }
 
 function start_registry() {
-  result=$(echo $@ | grep "\-\-upgrade")
-  if [[ "$result" != "" ]];then
+  echo "Step.6 start regisry [in process]"
+  checkupgrade="--upgrade"
+  if [[ $@ =~ $checkupgrade ]]; then
     echo "Step.6 upgrade will not start local registry"
     echo "Step.6 start registry [skip]"
   else
-    echo "Step.6 start regisry [in process]"
     mkdir -p $REGISTRY_DIR
-    nerdctl run $RegistryHTTPOptions "tkestack/registry-${ARCH}:$REGISTRY_VERSION" $@
-    nerdctl run $RegistryHTTPSOptions "tkestack/registry-${ARCH}:$REGISTRY_VERSION" $@
+    nerdctl run $RegistryHTTPOptions "tkestack/registry-${ARCH}:$REGISTRY_VERSION"
+    nerdctl run $RegistryHTTPSOptions "tkestack/registry-${ARCH}:$REGISTRY_VERSION"
     echo "Step.6 start registry [done]"
   fi
 }
