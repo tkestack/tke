6d78fa6ea9d8e46bf3a5335790fb3b7ab683011a
kanesong <kanesong@tencent.com>
2022-04-22 17:28:57 +0800

    perf(custom-proxy): disable keepalive to avoid 502 and timeout

--- pkg/platform/registry/cluster/storage/customresource_proxy.go
+++ pkg/platform/registry/cluster/storage/customresource_proxy.go
@@ -128,11 +128,13 @@ func (n *CustomResourceHandler) ServeHTTP(w http.ResponseWriter, r *http.Request
 		Host: strings.TrimPrefix(config.Host, "https://"),
 	})
 	reserveProxy.Transport = &http.Transport{
+		DisableKeepAlives: true,
 		DialContext: (&net.Dialer{
-			Timeout:   30 * time.Second,
+			Timeout:   5 * time.Second,
 			KeepAlive: 30 * time.Second,
 		}).DialContext,
-		MaxIdleConns:          100,
+		MaxIdleConns:          1000,
+		MaxIdleConnsPerHost:   100,
 		IdleConnTimeout:       90 * time.Second,
 		TLSHandshakeTimeout:   10 * time.Second,
 		ExpectContinueTimeout: 1 * time.Second,
