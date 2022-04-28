8caf16134804f34dbe7a0ead72d9ba7525a0e651
johnny19941216 <wuzhiqiang94@hotmail.com>
2022-04-28 16:36:05 +0800

    fix(console): fix clip cant use error

--- web/console/src/modules/common/components/clip/Clip.tsx
+++ web/console/src/modules/common/components/clip/Clip.tsx
@@ -15,63 +15,37 @@
  * WARRANTIES OF ANY KIND, either express or implied.  See the License for the
  * specific language governing permissions and limitations under the License.
  */
-import * as Clipboard from 'clipboard';
-import * as React from 'react';
 
-import { Bubble } from '@tea/component';
-import { BaseReactProps } from '@tencent/ff-redux';
-import { t, Trans } from '@tencent/tea-app/lib/i18n';
+import React, { useLayoutEffect, useState } from 'react';
+import { Copy, Icon } from 'tea-component';
 
-const tips = seajs.require('tips');
-export interface ClipProps extends BaseReactProps {
-  /**复制对象 */
-  target?: string;
-
-  /**是否显示 */
+export interface ClipProps {
+  target: string;
+  children?: React.ReactNode;
+  className?: string;
   isShow?: boolean;
-
-  /**是否显示操作提示 */
   isShowTip?: boolean;
-
-  /**提示方向 */
-  tipDirection?: 'top' | 'right' | 'left' | 'bottom';
 }
 
-export class Clip extends React.Component<ClipProps, {}> {
-  render() {
-    const { target, isShow = true, isShowTip, className, tipDirection, style, children } = this.props;
-    let renderClass = children ? 'copy-trigger ' + className : 'copy-trigger copy-icon ' + className;
-    return isShow ? (
-      <Bubble
-        // className={className}
-        placement={tipDirection || 'bottom'}
-        content={isShowTip ? t('复制') : null}
-      >
-        <a
-          href="javascript:;"
-          data-clipboard-action="copy"
-          data-clipboard-target={target}
-          className={renderClass}
-          style={style}
-        >
-          {children}
-        </a>
-      </Bubble>
-    ) : (
-      <noscript />
-    );
-  }
+export const Clip = ({ target, className, isShow = true, isShowTip }: ClipProps) => {
+  const [text, setText] = useState('');
 
-  componentDidMount() {
-    if (!window['paas_is_init_clipboard']) {
-      window['paas_is_init_clipboard'] = new Clipboard('.copy-trigger');
-      window['paas_is_init_clipboard'].on('success', e => {
-        tips.success(t('复制成功'), 1000);
-        e.clearSelection();
-      });
-      window['paas_is_init_clipboard'].on('error', e => {
-        tips.error(t('复制失败'), 1000);
-      });
+  useLayoutEffect(() => {
+    let targetText = '';
+    try {
+      targetText = document?.querySelector(target)?.textContent ?? '';
+    } catch (error) {
+      console.log(error);
     }
-  }
-}
+
+    setText(targetText);
+  }, [target]);
+
+  return (
+    isShow && (
+      <Copy text={text}>
+        <Icon type="copy" className={className} />
+      </Copy>
+    )
+  );
+};
