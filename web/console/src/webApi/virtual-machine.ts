b4457db7b9ea0553beece2be9ca043575a432b2e
johnny19941216 <wuzhiqiang94@hotmail.com>
2022-05-12 14:38:49 +0800

    feat(console): change network bridge model to virtio

--- web/console/src/webApi/virtual-machine.ts
+++ web/console/src/webApi/virtual-machine.ts
@@ -155,7 +155,7 @@ export function createVM({
 
               interfaces: [
                 {
-                  model: 'e1000',
+                  model: 'virtio',
                   name: 'default',
                   bridge: {}
                 }
