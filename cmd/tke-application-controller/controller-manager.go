a7c102a124b3ba9a2ffd3c1dd70fd62d432235f7
xdonggao <xdonggao@tencent.com>
2022-05-25 14:36:48 +0800

    feat(application): add application metrics for failed

--- cmd/tke-application-controller/controller-manager.go
+++ cmd/tke-application-controller/controller-manager.go
@@ -20,10 +20,13 @@ package main
 
 import (
 	"math/rand"
+	"net/http"
 	"os"
 	"runtime"
 	"time"
 
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+	"k8s.io/klog"
 	"tkestack.io/tke/cmd/tke-application-controller/app"
 )
 
@@ -32,6 +35,14 @@ func main() {
 	if len(os.Getenv("GOMAXPROCS")) == 0 {
 		runtime.GOMAXPROCS(runtime.NumCPU())
 	}
+
+	mux := http.NewServeMux()
+	mux.Handle("/metrics", promhttp.Handler())
+	go func() {
+		if err := http.ListenAndServe(":9464", mux); err != nil {
+			klog.Fatal(err)
+		}
+	}()
 
 	app.NewApp("tke-application-controller").Run()
 }
