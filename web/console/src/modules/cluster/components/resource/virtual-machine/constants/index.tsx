cc99dbaede7c04bdb1860b1a742c6ea4156258fd
johnny19941216 <wuzhiqiang94@hotmail.com>
2022-05-31 16:24:13 +0800

    feat(console): rewirite vm monitor chart

--- web/console/src/modules/cluster/components/resource/virtual-machine/constants/index.tsx
+++ web/console/src/modules/cluster/components/resource/virtual-machine/constants/index.tsx
@@ -65,76 +65,81 @@ export const VMDetailTabOptions = [
 ];
 
 const mean = str => `mean(${str})`;
-const sum = str => `sum(${str})`;
 
-export const vmMonitorFields: Array<{
-  expr: string;
-  alias: string;
-  unit: string;
-}> = [
+export const vmMonitorGroups = [
   {
-    expr: mean('vm_cpu_usage_rate'),
-    alias: 'CPU 使用率',
-    unit: '%'
+    by: ['name'],
+    fields: [
+      {
+        expr: mean('vm_cpu_usage_rate'),
+        alias: 'CPU 使用率',
+        unit: '%'
+      },
+
+      {
+        expr: mean('vm_memory_usage'),
+        alias: '内存使用量',
+        unit: 'MB'
+      },
+
+      {
+        expr: mean('vm_memory_usage_rate'),
+        alias: '内存使用率',
+        unit: '%'
+      },
+
+      {
+        expr: mean('vm_network_transmit_bw'),
+        alias: '网络入带宽',
+        unit: 'Mbps'
+      },
+
+      {
+        expr: mean('vm_network_receive_bw'),
+        alias: '网络出带宽',
+        unit: 'Mbps'
+      },
+
+      {
+        expr: mean('vm_network_transmit_packets_rate'),
+        alias: '网络入包量',
+        unit: '个/s'
+      },
+
+      {
+        expr: mean('vm_network_receive_packets_rate'),
+        alias: '网络出包量',
+        unit: '个/s'
+      }
+    ]
   },
 
   {
-    expr: mean('vm_memory_usage'),
-    alias: '内存使用量',
-    unit: 'MB'
-  },
+    by: ['name', 'drive'],
+    fields: [
+      {
+        expr: mean('vm_storage_read_bw'),
+        alias: '磁盘读流量',
+        unit: 'MB/s'
+      },
 
-  {
-    expr: mean('vm_memory_usage_rate'),
-    alias: '内存使用率',
-    unit: '%'
-  },
+      {
+        expr: mean('vm_storage_write_bw'),
+        alias: '磁盘写流量',
+        unit: 'MB/s'
+      },
 
-  {
-    expr: mean('vm_network_transmit_bw'),
-    alias: '网络入带宽',
-    unit: 'Mbps'
-  },
+      {
+        expr: mean('vm_storage_read_iops'),
+        alias: '磁盘读IOPS （基于每块磁盘）',
+        unit: '次/s'
+      },
 
-  {
-    expr: mean('vm_network_receive_bw'),
-    alias: '网络出带宽',
-    unit: 'Mbps'
-  },
-
-  {
-    expr: mean('vm_network_transmit_packets_rate'),
-    alias: '网络入包量',
-    unit: '个/s'
-  },
-
-  {
-    expr: mean('vm_network_receive_packets_rate'),
-    alias: '网络出包量',
-    unit: '个/s'
-  },
-
-  {
-    expr: mean('vm_storage_read_bw'),
-    alias: '磁盘读流量',
-    unit: 'MB/s'
-  },
-
-  {
-    expr: mean('vm_storage_write_bw'),
-    alias: '磁盘写流量',
-    unit: 'MB/s'
-  },
-
-  {
-    expr: mean('vm_storage_read_iops'),
-    alias: '磁盘读IOPS （基于每块磁盘）',
-    unit: '次/s'
-  },
-
-  {
-    expr: mean('vm_storage_write_iops'),
-    alias: '磁盘写IOPS （基于每块磁盘）',
-    unit: '次/s'
+      {
+        expr: mean('vm_storage_write_iops'),
+        alias: '磁盘写IOPS （基于每块磁盘）',
+        unit: '次/s'
+      }
+    ]
   }
 ];
