5a3ad56455ff8b077dc12278e1006cde9e8fcac4
GaoXiaodong <13814823514@163.com>
2022-08-04 10:13:16 +0800

    fix(application): restore metrics when app status is successed (#2050)
    Co-authored-by: xdonggao <xdonggao@tencent.com>

--- pkg/application/controller/app/action/install.go
+++ pkg/application/controller/app/action/install.go
@@ -119,6 +119,8 @@ func Install(ctx context.Context,
 			newStatus.Reason = ""
 			newStatus.LastTransitionTime = metav1.Now()
 			metrics.GaugeApplicationInstallFailed.WithLabelValues(app.Spec.TargetCluster, app.Name).Set(0)
+			metrics.GaugeApplicationUpgradeFailed.WithLabelValues(app.Spec.TargetCluster, app.Name).Set(0)
+			metrics.GaugeApplicationRollbackFailed.WithLabelValues(app.Spec.TargetCluster, app.Name).Set(0)
 		}
 		newApp, updateStatusErr = updateStatusFunc(ctx, newApp, &newApp.Status, newStatus)
 		if updateStatusErr != nil {
