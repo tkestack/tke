c9886fe54d8d8e0b67372859fa1cfd13f9f6c48d
wl-chen <wldklyx@qq.com>
2022-09-29 10:53:10 +0800

    feat(gateway): add tke namespace cluster-info comfigmap (#2097)

--- cmd/tke-installer/app/installer/installer.go
+++ cmd/tke-installer/app/installer/installer.go
@@ -273,10 +273,6 @@ func (t *TKE) initSteps() {
 			Name: "Install etcd",
 			Func: t.installETCD,
 		},
-		{
-			Name: "Patch platform versions in cluster info",
-			Func: t.patchPlatformVersion,
-		},
 	}...)
 
 	t.steps = append(t.steps, []types.Handler{
