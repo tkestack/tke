d5b4d9027ed9ad3a4aa237dcf440cc4a448d9bb7
johnny19941216 <wuzhiqiang94@hotmail.com>
2022-12-07 17:12:00 +0800

    feat(console): change DJB hash to md5

--- web/console/webpack/csrf.js
+++ web/console/webpack/csrf.js
@@ -1,14 +1,7 @@
-function DJBHash(e) {
-  let t = 5381;
-  for (let n = 0, r = e.length; n < r; ++n) {
-    t += (t << 5) + e.charCodeAt(n);
-  }
+const SparkMD5 = require('spark-md5');
 
-  return t & 0x7fffffff;
-}
-
-function createCsrfCode(cookieStr) {
-  const cookie = cookieStr.split('; ').reduce((all, item) => {
+function parseCookie(cookieStr) {
+  cookieStr.split('; ').reduce((all, item) => {
     const [key, value] = item.split('=');
 
     return {
@@ -16,16 +9,20 @@ function createCsrfCode(cookieStr) {
       [key]: value
     };
   }, {});
+}
 
-  const tkeCookie = cookie?.['tke'];
+function createCSRFHeader(cookieStr) {
+  const cookie = parseCookie(cookieStr);
 
-  if (!tkeCookie) return 0;
+  const tkeCookie = cookie?.['tke'] ?? '';
 
-  const code = DJBHash(tkeCookie);
+  const token = SparkMD5.hash(tkeCookie);
 
-  return code;
+  return {
+    'X-CSRF-TOKEN': token
+  };
 }
 
 module.exports = {
-  createCsrfCode
+  createCSRFHeader
 };
