d8e63211effa88da1e3f03fc81947f73e41ce29c
Leo Ryu <leonarliu@tencent.com>
2023-04-06 10:52:55 +0800

    fix(installer): add insecure-registry (#2258)

--- build/docker/tools/tke-installer/dockerd-entrypoint.sh
+++ build/docker/tools/tke-installer/dockerd-entrypoint.sh
@@ -139,6 +139,7 @@ if [ "$#" -eq 0 ] || [ "${1#-}" != "$1" ]; then
 			--tlscacert "$DOCKER_TLS_CERTDIR/server/ca.pem" \
 			--tlscert "$DOCKER_TLS_CERTDIR/server/cert.pem" \
 			--tlskey "$DOCKER_TLS_CERTDIR/server/key.pem" \
+			--insecure-registry=registry.tke.com \
 			"$@"
 		DOCKERD_ROOTLESS_ROOTLESSKIT_FLAGS="${DOCKERD_ROOTLESS_ROOTLESSKIT_FLAGS:-} -p 0.0.0.0:2376:2376/tcp"
 	else
@@ -146,6 +147,7 @@ if [ "$#" -eq 0 ] || [ "${1#-}" != "$1" ]; then
 		set -- dockerd \
 			--host="$dockerSocket" \
 			--host=tcp://0.0.0.0:2375 \
+			--insecure-registry=registry.tke.com \
 			"$@"
 		DOCKERD_ROOTLESS_ROOTLESSKIT_FLAGS="${DOCKERD_ROOTLESS_ROOTLESSKIT_FLAGS:-} -p 0.0.0.0:2375:2375/tcp"
 	fi
