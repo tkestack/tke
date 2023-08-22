537798f8dd19bd1126d1b97f0d98624880dbb764
GaoXiaodong <13814823514@163.com>
2023-08-22 18:09:37 +0800

    fix(platform): watch cluster/clustercredential/application with options (#2307)
    Co-authored-by: xdonggao <xdonggao@tencent.com>

--- pkg/platform/registry/clustercredential/storage/storage.go
+++ pkg/platform/registry/clustercredential/storage/storage.go
@@ -25,12 +25,14 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/fields"
 	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/watch"
 	genericregistry "k8s.io/apiserver/pkg/registry/generic"
 	"k8s.io/apiserver/pkg/registry/generic/registry"
 	"k8s.io/apiserver/pkg/registry/rest"
 	platforminternalclient "tkestack.io/tke/api/client/clientset/internalversion/typed/platform/internalversion"
 	"tkestack.io/tke/api/platform"
 	"tkestack.io/tke/pkg/apiserver/authentication"
+	apiserverutil "tkestack.io/tke/pkg/apiserver/util"
 	"tkestack.io/tke/pkg/platform/registry/clustercredential"
 	"tkestack.io/tke/pkg/platform/util"
 	"tkestack.io/tke/pkg/util/log"
@@ -94,6 +96,12 @@ var _ rest.ShortNamesProvider = &REST{}
 // ShortNames implements the ShortNamesProvider interface. Returns a list of short names for a resource.
 func (r *REST) ShortNames() []string {
 	return []string{"cc"}
+}
+
+// Watch selects resources in the storage which match to the selector. 'options' can be nil.
+func (r *REST) Watch(ctx context.Context, options *metainternal.ListOptions) (watch.Interface, error) {
+	wrappedOptions := apiserverutil.PredicateListOptions(ctx, options)
+	return r.Store.Watch(ctx, wrappedOptions)
 }
 
 // List selects resources in the storage which match to the selector. 'options' can be nil.
